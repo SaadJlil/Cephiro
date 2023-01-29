27bc082bb3437b49a0321a5336e46980786d452c
Ubuntu <azureuser@AzureLearning.fnq0yvfqusmu1mheqy14vxermf.bx.internal.cloudapp.net>
2023-01-29 23:28:02 +0000

    More Progress on dapper

--- Backend/Cephiro.Identity/Infrastructure/Accessor/UsersAccess.cs
+++ Backend/Cephiro.Identity/Infrastructure/Accessor/UsersAccess.cs
@@ -54,42 +54,68 @@ public sealed class UserAccess: IUserAccess
     }
     public async Task<UserIdentityInfoDto?> GetUserInfoById(Guid Id, CancellationToken cancellation)
     {
+        User result;
+        try{
+            string sqlQuery = $@"SELECT * FROM users WHERE id = @Id LIMIT 1";
 
-        string sqlQuery = $@"SELECT * FROM users WHERE id = @Id LIMIT 1";
+            var query = new CommandDefinition(
+                commandText: sqlQuery, 
+                parameters: new { Id },
+                cancellationToken: cancellation);
 
-        var query = new CommandDefinition(
-            commandText: sqlQuery, 
-            parameters: new { Id },
-            cancellationToken: cancellation);
+            using(NpgsqlConnection db = new NpgsqlConnection(_settings.CurrentValue.IdentityConnection))
+            {
+                db.Open();
+                result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+                db.Close();
+            }
 
-        var result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+            return new UserIdentityInfoDto(
+                Id: result.Id,
+                FirstName: result.FirstName,
+                Email: result.Email
+            );
+        }
+        catch(NpgsqlException exception){
 
-        return new UserIdentityInfoDto(
-            Id: result.Id,
-            FirstName: result.FirstName,
-            Email: result.Email
-        );
+        }
+
+
     }
 
     public async Task<UserIdentityInfoDto?> GetUserInfoByEmail(string Email, CancellationToken cancellation)
     {
-        string sqlQuery = $@"SELECT * FROM users WHERE email = @Email LIMIT 1";
+        User result;
+        try{
+            string sqlQuery = $@"SELECT * FROM users WHERE email = @Email LIMIT 1";
 
-        var query = new CommandDefinition(
-            commandText: sqlQuery, 
-            parameters: new { Email },
-            cancellationToken: cancellation);
+            var query = new CommandDefinition(
+                commandText: sqlQuery, 
+                parameters: new { Email },
+                cancellationToken: cancellation);
 
-        var result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+            using(NpgsqlConnection db = new NpgsqlConnection(_settings.CurrentValue.IdentityConnection))
+            {
+                db.Open();
+                result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+                db.Close();
+            }
 
-        return new UserIdentityInfoDto(
-            Id: result.Id,
-            FirstName: result.FirstName,
-            Email: result.Email
-        );
+            return new UserIdentityInfoDto(
+                Id: result.Id,
+                FirstName: result.FirstName,
+                Email: result.Email
+            );
+        }
+        catch(NpgsqlException exception){
+
+        }
+
+
     }
     public async Task<UserIdentityInfoDto?> GetUserInfoByPhone(string PhoneNumber, CancellationToken cancellation)
     {
+        User result;
         try{
             string sqlQuery = $@"SELECT * FROM users WHERE phone_number = @PhoneNumber LIMIT 1";
 
@@ -98,7 +124,12 @@ public sealed class UserAccess: IUserAccess
                 parameters: new { PhoneNumber },
                 cancellationToken: cancellation);
 
-            var result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+            using(NpgsqlConnection db = new NpgsqlConnection(_settings.CurrentValue.IdentityConnection))
+            {
+                db.Open();
+                result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+                db.Close();
+            }
 
             return new UserIdentityInfoDto(
                 Id: result.Id,
@@ -106,9 +137,10 @@ public sealed class UserAccess: IUserAccess
                 Email: result.Email
             );
         }
-        catch(NpgsqlException){
+        catch(NpgsqlException exception){
 
         }
+
 
     }
 
@@ -119,47 +151,68 @@ public sealed class UserAccess: IUserAccess
     //Profile
     public async Task<IEnumerable<UserProfileDto?>> GetAllUsersProfile(CancellationToken cancellation)
     {
+        
+        IEnumerable<User> result;
+        try{
+            string sqlQuery = $@"SELECT * FROM users";
 
-        string sqlQuery = $@"SELECT * FROM users";
+            var query = new CommandDefinition(
+                commandText: sqlQuery, 
+                cancellationToken: cancellation);
 
-        var query = new CommandDefinition(
-            commandText: sqlQuery, 
-            cancellationToken: cancellation
-        );
+            using(NpgsqlConnection db = new NpgsqlConnection(_settings.CurrentValue.IdentityConnection))
+            {
+                db.Open();
+                result = await _connect.QueryAsync<User>(query);
+                db.Close();
+            }
 
-        var result = await _connect.QueryAsync<User>(query);
+            return result.Select(x => new UserProfileDto(
+                x.ImageUri,
+                x.MiddleName != "" ? $"{x.FirstName} {x.MiddleName} {x.LastName}" : $"{x.FirstName} {x.LastName}",
+                x.Email,
+                x.PhoneNumber ?? "",
+                x.Description ?? "",
+                x.RegularizationStage
+            ));
 
-        return result.Select(x => new UserProfileDto(
-            x.ImageUri,
-            x.MiddleName != "" ? $"{x.FirstName} {x.MiddleName} {x.LastName}" : $"{x.FirstName} {x.LastName}",
-            x.Email,
-            x.PhoneNumber ?? "",
-            x.Description ?? "",
-            x.RegularizationStage
-        ));
+       }
+        catch(NpgsqlException exception){
+
+        }
     }
     public async Task<UserProfileDto?> GetUserProfileById(Guid Id, CancellationToken cancellation)
     {
+        User result;
+        try{
+            string sqlQuery = $@"SELECT * FROM users WHERE id = @Id LIMIT 1";
 
-        string sqlQuery = $@"SELECT * FROM users WHERE id = @Id LIMIT 1";
+            var query = new CommandDefinition(
+                commandText: sqlQuery, 
+                parameters: new { Id },
+                cancellationToken: cancellation);
 
-        var query = new CommandDefinition(
-            commandText: sqlQuery, 
-            parameters: new { Id },
-            cancellationToken: cancellation);
+            using(NpgsqlConnection db = new NpgsqlConnection(_settings.CurrentValue.IdentityConnection))
+            {
+                db.Open();
+                result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+                db.Close();
+            }
 
-        var result = await _connect.QueryFirstOrDefaultAsync<User>(query);
+            return new UserProfileDto(
+                result.ImageUri,
+                result.MiddleName != "" ? $"{result.FirstName} {result.MiddleName} {result.LastName}" : $"{result.FirstName} {result.LastName}",
+                result.Email,
+                result.PhoneNumber ?? "",
+                result.Description ?? "",
+                result.RegularizationStage
+            );
+       }
+        catch(NpgsqlException exception){
 
-        return new UserProfileDto(
-            result.ImageUri,
-            result.MiddleName != "" ? $"{result.FirstName} {result.MiddleName} {result.LastName}" : $"{result.FirstName} {result.LastName}",
-            result.Email,
-            result.PhoneNumber ?? "",
-            result.Description ?? "",
-            result.RegularizationStage
-        );
+        }
 
-    }
+   }
 
     public async Task<UserProfileDto?> GetUserProfileByEmail(string Email, CancellationToken cancellation)
     {
