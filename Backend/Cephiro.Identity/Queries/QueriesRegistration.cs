11a295954bf4b08c0ebafcea6c7176e5a1147d26
Ubuntu <azureuser@AzureLearning.fnq0yvfqusmu1mheqy14vxermf.bx.internal.cloudapp.net>
2023-01-31 16:34:55 +0000

    Consumer Configuration

---
+++ Backend/Cephiro.Identity/Queries/QueriesRegistration.cs
@@ -0,0 +1,19 @@
+using MassTransit;
+using System.Reflection;
+using Cephiro.Identity.Contracts.Request.Info;
+using Cephiro.Identity.Contracts.Request.Profile;
+
+
+public static class QueriesRegistration 
+{
+    public static void AddQueriesRegistration(this IMediatorRegistrationConfigurator cfg)
+    {
+        cfg.AddConsumers(Assembly.GetExecutingAssembly());
+
+        //Info
+        cfg.AddRequestClient<UserInfoRequest>();
+        
+        //Profile
+        cfg.AddRequestClient<UserProfileRequest>();
+   }
+}
